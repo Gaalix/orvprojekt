3800ac782b9552e3c4188abdebd881f3da814c85
Gal Borinc <borincgal2003@gmail.com>
2024-06-09 11:19:01 +0200

    KN-42 added tests and deploy script

--- model/test_model.py
+++ model/test_model.py
@@ -1,26 +1,42 @@
+import unittest
+from unittest import mock
 import tensorflow as tf
+from keras.src.callbacks import EarlyStopping
 from keras.src.legacy.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout
+import keras_tuner as kt
+import model.model as mm
 
-def load_test_data(test_data_dir):
-    datagen = ImageDataGenerator()
-    test_generator = datagen.flow_from_directory(test_data_dir, target_size=(224, 224), batch_size=100, class_mode='binary')
-    return test_generator
+class TestModel(unittest.TestCase):
 
-def test_model(model_path, test_data_dir):
-    # Load the saved model
-    model = tf.keras.models.load_model(model_path)
+    @mock.patch('tensorflow.keras.applications.MobileNetV2')
+    @mock.patch('tensorflow.keras.optimizers.Adam')
+    @mock.patch('tensorflow.keras.models.Model')
+    def model_building(self, mock_model, mock_adam, mock_mobilenet):
+        mock_mobilenet.return_value = mock.Mock()
+        mock_adam.return_value = mock.Mock()
+        mock_model.return_value = mock.Mock()
+        hp = kt.HyperParameters()
+        mm.build_model(hp)
+        mock_mobilenet.assert_called_once()
+        mock_adam.assert_called_once()
+        mock_model.assert_called_once()
 
-    # Load the test data
-    test_generator = load_test_data(test_data_dir)
+    @mock.patch('keras.src.legacy.preprocessing.image.ImageDataGenerator')
+    def data_loading(self, mock_datagen):
+        mock_datagen.return_value = mock.Mock()
+        mm.load_data('data_dir')
+        mock_datagen.assert_called_once()
 
-    # Evaluate the model
-    loss, accuracy = model.evaluate(test_generator)
+    @mock.patch('keras_tuner.BayesianOptimization')
+    @mock.patch('tensorflow.keras.callbacks.EarlyStopping')
+    def hyperparameters_tuning(self, mock_early_stopping, mock_bayesian_optimization):
+        mock_early_stopping.return_value = mock.Mock()
+        mock_bayesian_optimization.return_value = mock.Mock()
+        mm.tune_hyperparameters('train_generator', 'val_generator')
+        mock_early_stopping.assert_called_once()
+        mock_bayesian_optimization.assert_called_once()
 
-    print(f"Test loss: {loss}")
-    print(f"Test accuracy: {accuracy}")
-
-# Usage
-test_model("user_models/model_2.keras", "../testData/")
-
-#0 = user
-#1 = unknown
+if __name__ == '__main__':
+    unittest.main()
